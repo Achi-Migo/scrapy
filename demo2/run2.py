fe77b7be57756bd45ed540ff5bf42ca9ec16c5da
cjj <1096923379@qq.com>
2020-07-12 13:13:24 +0800

    2020年7月10日23:59:15

--- demo2/run2.py
+++ demo2/run2.py
@@ -13,7 +13,7 @@ def down_m3u8(m3u8, title):
     cmd = ffmpeg + " -i " + m3u8 + " -n -c copy " + cache_path + title
     x = exec(cmd)
     if str(x).__contains__('HTTP error 404 Not Found'):
-        utils.update_already(title, m3u8,404)
+        utils.update_already(title, m3u8, 404)
     else:
         utils.update_already(title, m3u8)
 
@@ -28,16 +28,21 @@ def exec(cmd):
 
 def ffmpeg_down():
     avhub_list = utils.select_url()
-    cache_list = []
-    for f in os.listdir(cache_path):
-        f = f.replace(" ", "")
-        if f.endswith('.mp4') and os.path.getsize(cache_path + f) > 0:
-            cache_list.append(f)
-    for a in avhub_list:
-        title = a['title']
-        if not cache_list.__contains__(title):
-            down_m3u8(a['m3u8_url'], title)
-            pass
+    while True:
+        if avhub_list.__len__() == 0:
+            break
+
+        cache_list = []
+        for f in os.listdir(cache_path):
+            f = f.replace(" ", "")
+            if f.endswith('.mp4') and os.path.getsize(cache_path + f) > 0:
+                cache_list.append(f)
+        avhub_list = utils.select_url()
+        for a in avhub_list:
+            title = a['title']
+            if not cache_list.__contains__(title):
+                down_m3u8(a['m3u8_url'], title)
+                break
 
 
 if __name__ == '__main__':
